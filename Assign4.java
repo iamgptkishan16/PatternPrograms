8e12d3df3feb2fa328f53ee8aedb496408d0ca83
iamgptkishan16 <73187332+iamgptkishan16@users.noreply.github.com>
2022-10-26 15:43:12 +0530

    Add files via upload

---
+++ Assign4.java
@@ -0,0 +1,19 @@
+public class Assign4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 15;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+			  if((j == 0 && i > (n-1)/2) || (j == n-1 && i > (n-1)/2) || (i-j >= (n-1)/2) || (i+j >= (n-1)+(n-1)/2) || i == n-1) {
+				  System.out.print("*");
+			  } else {
+				  System.out.print(" ");
+			  }
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ NamePattern.java
@@ -0,0 +1,61 @@
+
+public class NamePattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 13;
+		for(int i = 0; i<n; i++) {
+			for(int j = 0; j<n; j++) {
+				if(j == 0 || i+j == (n-1)/2 || i - j == (n-1)/2) {
+					System.out.print("* ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+			
+
+			for(int j = 0; j<n; j++) {
+				if(i == 0 || i == n-1 || j == (n-1)/2) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+			for(int j = 0; j<n; j++) {
+				if((j==0 && i > 0 && i < (n-1)/2) || (j==n-1 && i > (n-1)/2 && i < n-1) || (i == 0 && j > 0 && j < n-1) || (i == n-1 && j > 0 && j < n-1) || (i == (n-1)/2 && j > 0 && j < n-1)) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+			for(int j = 0; j<n; j++) {
+				if(j==0 || j == n-1 || i == (n-1)/2) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+			for(int j = 0; j<n; j++) {
+				if((j == 0 && i > 0) || (j == n-1 && i > 0) || i == (n-1)/2 || (i == 0 && j > 0 && j < n-1)) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+			for(int j = 0; j<n; j++) {
+				if(j == 0 || j == n-1 || i == j) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
